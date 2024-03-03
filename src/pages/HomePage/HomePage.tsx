97c5dfd7701e86828fb0fc3f980d4b4acec59403
andressafirmino <andressafirmino78@gmail.com>
2024-03-03 07:16:00 -0300

    feat: get list of employees

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,8 +1,21 @@
+/// <reference types="node" />
+import { useEffect, useState } from 'react';
 import EmployeesTable from '../../components/EmployeesTable/EmployeesTable';
 import SearchBox from '../../components/Search/Search';
+import { getEmployees } from '../../services/homeApi';
 import { HomeContainer } from './style';
 
 export default function HomePage() {
+  const [employees, setEmployees] = useState(null);
+  console.log(employees);
+  useEffect(() => {
+    async function fetchEmployees() {
+      const response = await getEmployees();
+      setEmployees(response);
+      console.log(response);
+    }
+    fetchEmployees();
+  }, []);
   return (
     <HomeContainer>
       <SearchBox />
